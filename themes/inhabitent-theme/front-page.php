58fa3db0d4f1d51e764bf085f0e71f12b7269452
Kurtis Young <iBeast@Kurtiss-MacBook-Pro.local>
2016-05-20 11:51:20 -0700

    first commit,made front-page.php and fixed header/navigation

---
+++ themes/inhabitent-theme/front-page.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for displaying the front page.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<!-- Hero Image -->
+
+				<section class="home-hero">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/inhabitent-logo-full.svg" class="logo" alt="Inhabitent Logo" />
+				</section>
+
+			<!-- Shop Stuff -->
+
+				<!-- Loop goes here -->
+
+			<!-- Inhabitent Journal -->
+
+				<!-- Loop goes here -->
+
+			<!-- Latest Adventures -->
+
+				<!-- Stretch goal - Mandi used floats -->
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
