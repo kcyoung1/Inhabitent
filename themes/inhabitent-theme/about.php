34ab3bc338e05fb4f677025e5516ccb4a64894a0
Kurtis Young <iBeast@Kurtiss-MacBook-Pro.local>
2016-05-21 23:13:47 -0700

    added font family to headers and editing footer

---
+++ themes/inhabitent-theme/about.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * About page
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+<div class="page-content">
+	<div id="primary" class="content-area journal-area">
+		<main id="main" class="site-main" role="main">
+
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>
--- themes/inhabitent-theme/footer.php
+++ themes/inhabitent-theme/footer.php
@@ -15,18 +15,34 @@
 						<div class="contact-info">
 							<h3>Contact Info</h3>
 							<p>
-								
+								<i class="fa fa-envelope-o" aria-hidden="true"></i>
+								<a href="emailto:info@inhabitent.com">info@inhabitent.com</a>
+							</p>
+							<p>
+								<a href="tel:5553434567891">778-456-7891</a>
+							</p>
+							<p>
+								fb twitter google+
 							</p>
 						</div>
 					</div>
 					<div class="block">
 						<div class="business-hours">
-
+							<h3>Business Hours</h3>
+							<p>
+								<span class="days">Monday-Friday:</span> 9am to 5pm
+							</p>
+							<p>
+								<span class="days">Saturday:</span> 10am to 2pm
+							</p>
+							<p>
+								<span class="days">Sunday:</span> Closed
+							</p>
 						</div>
 					</div>
 					<div class="block">
 						<div class="footer-logo">
-
+							<a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/inhabitent-logo-text.svg" alt="Inhabitent Logo" /></a>
 						</div>
 					</div>
 				</div>
